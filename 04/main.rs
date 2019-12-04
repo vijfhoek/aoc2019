518de18a427a1e8ced5ed51f48d77e5cf56df4a8
Sijmen Schoon <me@sijmenschoon.nl>
2019-12-04 09:26:29 +0100

    Small optimization

--- 04/main.rs
+++ 04/main.rs
@@ -1,20 +1,21 @@
 #![feature(const_fn)]
 
-fn filter(p: &String) -> bool {
-    let bytes = p.as_bytes();
+fn filter(p: &u32) -> bool {
+    let string = p.to_string();
+    let bytes = string.as_bytes();
     let mut double = 0;
     let mut prev_double = 0;
 
     for i in 0..5 {
         if bytes[i + 1] < bytes[i] {
-            return false
+            return false;
         }
 
         if bytes[i] == bytes[i + 1] {
             if double == bytes[i] {
                 prev_double = double;
                 double = 0;
-            } else if prev_double != bytes[i] && double == 0{
+            } else if prev_double != bytes[i] && double == 0 {
                 double = bytes[i]
             }
         }
@@ -27,9 +28,6 @@ fn main() {
     let from = 100000;
     let to = 999999;
 
-    let possibilities = (from..to)
-        .map(|p| p.to_string())
-        .filter(filter)
-        .count();
+    let possibilities = (from..to).filter(filter).count();
     dbg!(possibilities);
 }
